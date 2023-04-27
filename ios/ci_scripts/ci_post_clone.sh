87e925fa89538fbbe85b2d3e7904baf55e8f77a8
TEK <oluwabishefiu@gmail.com>
2023-04-27 16:34:15 +0200

    test build

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -13,6 +13,7 @@ brew install yarn
 yarn
 pod install
 # remove accessibilyi
-rm -r /Volumes/workspace/repository/ios/Pods/React-Core-AccessibilityResources
+# /Users/tek/Documents/Projects/ReactNative/NativeProjects/EaziInvest/ios/Pods/Pods.xcodeproj/xcuserdata/tek.xcuserdatad
+# rm -r /Volumes/workspace/repository/ios/Podszoj/xcuserdata/tek.xcuserdatad/React-Core-AccessibilityResources
 # the sed command from RN cant find the file... so we have to run it ourselves
 sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
